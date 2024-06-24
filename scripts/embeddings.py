742e76da2ace6b32ab1b9498983ed9521bbf4a01
MarleneSchaff <mschaf11@jh.edu>
2024-06-24 11:49:58 -0400

    initial commit

---
+++ scripts/embeddings.py
@@ -0,0 +1,58 @@
+import argparse
+import gzip
+import json
+
+import logging
+
+from transformers import AutoTokenizer, AutoModel
+from torch.nn.functional import softmax
+import re
+import torch
+import csv
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("samples_in", help="tokenized gz.jsonl file containing fulltext and metadata")
+	parser.add_argument("embeddings_out", help="csv with embeddings and texts")
+	parser.add_argument("--model", help="bert or roberta model name")
+	parser.add_argument("--target_words", nargs="+", help="Embed all sents containing these words. Case insensitive")
+	parser.add_argument("--animate_pronouns", nargs="+", help="list of animate pronouns")
+	parser.add_argument("--inanimate_pronouns", nargs="+", help="list of inanimate pronouns")
+
+	a_t = AutoTokenizer.from_pretrained(args.model)
+	model = AutoModel.from_pretrained(args.model)
+
+	args, other = parser.parse_known_args()
+	print(args.target_words)
+	search_pattern = re.compile(r"\b(?:%s)\b" % "|".join(args.target_words), re.IGNORECASE)
+	inputs_animate = a_t(" ".join(args.animate_pronouns), return_tensors = "pt", add_special_tokens=False).input_ids
+	inputs_inanimate = a_t(" ".join(args.inanimate_pronouns), return_tensors = "pt", add_special_tokens=False).input_ids
+
+	with gzip.open(args.samples_in, "rt") as in_s, open(args.embeddings_out, "wt", newline="") as e_out:
+		out_writer=csv.writer(e_out)
+		out_writer.writerow(["ID","Title","Author","Sentence","Word","Score"])
+		n=0
+		for line in in_s:
+			if n<1000000000:
+				sent_embeds = []
+				j_line = json.loads(line)
+				for sent in j_line["full_text"]:
+					if re.search(search_pattern, sent):
+						for w in args.target_words:
+							masked_sentence = re.subn(r"\b(?:%s)\b" % w, tokenizer.mask_token, sent, re.IGNORECASE)
+							if masked_sentence[1] > 0:
+								print(masked_sentence[0])
+								try:
+									tokenized_sent = a_t(masked_sentence[0], return_tensors="pt")
+									with torch.no_grad():
+										logits = model(**tokenized_sent).logits
+									mask_token_index = (tokenized_sent.input_ids == a_t.mask_token_id)[0].nonzero(as_tuple=True)[0]
+									masked_token_logits = logits[0, mask_token_index]
+									print(masked_token_logits.shape)
+									pdf = softmax(masked_token_logits, dim=-1)
+									a_score = torch.log(torch.sum(pdf[0, inputs_animate]))-torch.log(torch.sum(pdf[0, inputs_inanimate])).item()
+									print(a_score)
+									out_writer.writerow([j_line["id"],j_line["title"], j_line["author"], sent, w, a_score.item()])
+								except RuntimeError:
+									out_writer.writerow([j_line["id"],j_line["title"], j_line["author"], sent, w, "error"])
+			n=n+1
