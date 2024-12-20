c3590e135bfc6f5dbd5b64d267bf919846d0a584
MarleneSchaff <mschaf11@jh.edu>
2024-12-20 16:29:11 -0600

    adding spacy

--- scripts/dependent_verbs.py
+++ scripts/dependent_verbs.py
@@ -18,18 +18,36 @@ if __name__ == "__main__":
 		out_writer.writerow(["title", "author", "id", "sentence", "masked", "word", "score", "verbs", "dependents"])
 		for line in v_in:
 			jline = json.loads(line)
-#			print("loaded jline")
-			doc = nlp(jline["masked"][0])
-#			print("loaded doc")
-			for token in doc:
+			doc1 = nlp(jline["masked"][0])
+			doc2 = nlp(jline["sentence"])
+			for chunk in doc1.noun_chunks:
+				if chunk.root.text == "mask":
+#					print(chunk.root.head.text)
+					mask_token = chunk.root.text
+					print(mask_token)
+					if chunk.root.head.pos_ == "VERB":
+						dep_verb = chunk.root.head.text
+						print(dep_verb)
+					elif chunk.root.head.pos_ != "VERB":
+						head_token = chunk.root.head
+						if head_token.root.head.text.pos_ == "VERB":
+							dep_verb_two = head_token.root.head.text
+							print(dep_verb_two)
+						else:
+							print("no verb found")
+#					out_writer.writerow(mask_token, dep_verb, dep_verb_two)
+
+#					out_writer.writerow([jline["title"], jline["author"], jline["id"], jline["sentence"], jline["masked"], jline["word"], jline["score"], jline["verbs"], mask_token, dep_verb, dep_verb_two])
+
+
 #				print(token)
-				if token.text == "mask":
+#				if token.text == "mask":
 #					print("target word found")
-					print(token.text)
-					for child in token.children:
-						print(child)
-						if child.pos_ == "VERB":
-							print(child)
+#					print(token.text)
+#					for child in token.children:
+#						print(child)
+#						if child.pos_ == "VERB":
+#							print(child)
 #							input()
-							dependents = child.text
-							out_writer.writerow([jline["title"], jline["author"], jline["id"], jline["sentence"], jline["masked"], jline["word"], jline["score"], jline["verbs"], dependents])
+#							dependents = child.text
+#							out_writer.writerow([jline["title"], jline["author"], jline["id"], jline["sentence"], jline["masked"], jline["word"], jline["score"], jline["verbs"], dependents])
