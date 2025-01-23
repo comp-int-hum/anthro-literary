28e50095673ab8732b25f76bc708981ffb4c081e
MarleneSchaff <mschaf11@jh.edu>
2025-01-23 10:34:14 +0000

    update to scripts (dependencies)

--- scripts/dependent_verbs.py
+++ scripts/dependent_verbs.py
@@ -6,48 +6,44 @@ import spacy
 
 nlp = spacy.load("en_core_web_sm")
 
+#this should run on embeddings sentences not verbs?
+
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
-	parser.add_argument("verbs_in", help="gz.jsonl with nouns and verbs")
+	parser.add_argument("embed_in", help="gz.jsonl with embeddings")
+	parser.add_argument("target_words", help="text file with target words")
 	parser.add_argument("dependencies_out", help="csv with nouns and matching dependent verbs")
 
 	args = parser.parse_args()
 
-	with gzip.open(args.verbs_in, "rt") as v_in, open(args.dependencies_out, "wt", newline="") as d_out:
+	with gzip.open(args.embed_in, "rt") as e_in, open(args.target_words, "r") as t_w, open(args.dependencies_out, "wt", newline="") as d_out:
 		out_writer = csv.writer(d_out)
-		out_writer.writerow(["title", "author", "id", "sentence", "masked", "word", "score", "verbs", "dependents"])
-		for line in v_in:
+#		out_writer.writerow(["title", "author", "id", "sentence", "masked", "word", "score", "verbs", "dependents"])
+		for line in e_in:
 			jline = json.loads(line)
-			doc1 = nlp(jline["masked"][0])
-			doc2 = nlp(jline["sentence"])
-			for chunk in doc1.noun_chunks:
-				if chunk.root.text == "mask":
-#					print(chunk.root.head.text)
-					mask_token = chunk.root.text
-					print(mask_token)
-					if chunk.root.head.pos_ == "VERB":
-						dep_verb = chunk.root.head.text
-						print(dep_verb)
-					elif chunk.root.head.pos_ != "VERB":
-						head_token = chunk.root.head
-						if head_token.root.head.text.pos_ == "VERB":
-							dep_verb_two = head_token.root.head.text
-							print(dep_verb_two)
-						else:
-							print("no verb found")
-#					out_writer.writerow(mask_token, dep_verb, dep_verb_two)
+			doc = nlp(jline["sentence"])
+			root = [token for token in doc if token.dep_ == "ROOT"][0]
+			verbs = [child.text for child in root.children if child.pos_ == "VERB"]
+			for sent in doc.sents:
+				for w in t_w:
+					for token in sent:
+						if token.text == w:
+							target = token
+							head_one = target.head
+							if head_one.pos_ == "VERB":
+								print("target found first")
+#								dependents = head_one.text
+								#write out with "dependents": head_one.text
+							else:
+								head_two = head_one.head
+								if head_two.pos_ == "VERB":
+									print("target verb found second")
+#									dependents = head_two.text
+									#write out with "dependents": head_two.text
+								else:
+									print("nothing found")
+									#write out with "dependents": "nothing found"
 
-#					out_writer.writerow([jline["title"], jline["author"], jline["id"], jline["sentence"], jline["masked"], jline["word"], jline["score"], jline["verbs"], mask_token, dep_verb, dep_verb_two])
+#					out_writer.writerow([jline["title"], jline["author"], jline["id"], jline["sentence"], jline["masked"], jline["word"], jline["score"], jline["verbs"], dependents])
 
-
-#				print(token)
-#				if token.text == "mask":
-#					print("target word found")
-#					print(token.text)
-#					for child in token.children:
-#						print(child)
-#						if child.pos_ == "VERB":
-#							print(child)
-#							input()
-#							dependents = child.text
-#							out_writer.writerow([jline["title"], jline["author"], jline["id"], jline["sentence"], jline["masked"], jline["word"], jline["score"], jline["verbs"], dependents])
+#should be gz.jsonl instead of csv?
