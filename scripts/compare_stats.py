f81116b938624bf5f593e1474b706ea020670577
MarleneSchaff <mschaf11@jh.edu>
2025-01-24 09:14:20 +0000

    update to statistical comparison script

--- scripts/compare_stats.py
+++ scripts/compare_stats.py
@@ -13,33 +13,26 @@ if __name__ == "__main__":
 
 	args = parser.parse_args()
 
-	bert_df = pd.read_json(args.bert_in, lines=True, compression="gzip")
-	rob_df = pd.read_json(args.roberta_in, lines=True, compression="gzip")
+	with gzip.open(args.bert_in, "rt") as b_in, gzip.open(args.roberta_in, "rt") as r_in:
+		bert_loaded_json = [{"score": json.loads(line)["score"]} for line in b_in]
+		rob_loaded_json = [{"score": json.loads(line)["score"]} for line in r_in]
+		bert_df = pd.DataFrame(bert_loaded_json)
+		rob_df = pd.DataFrame(rob_loaded_json)
 
-	bert_df["score"] = pd.to_numeric(bert_df["score"], errors="coerce")
-	rob_df["score"] = pd.to_numeric(rob_df["score"], errors="coerce")
+		bert_df["score"] = pd.to_numeric(bert_df["score"], errors="coerce")
+		rob_df["score"] = pd.to_numeric(rob_df["score"], errors="coerce")
 
-	bert_mean, bert_var, bert_std = stats.mvsdist(bert_df["score"])
-	rob_mean, rob_var, rob_std = stats.mvsdist(rob_df["score"])
-	correlation, p_val = stats.pearsonr(rob_df["score"], bert_df["score"])
+		bert_df["score"].dropna()
+		rob_df["score"].dropna()
 
-	out_df = pd.DataFrame()
-	print("done")
+		bert_mean, bert_var, bert_std = stats.mvsdist(bert_df["score"])
+		rob_mean, rob_var, rob_std = stats.mvsdist(rob_df["score"])
+		correlation, p_val = stats.pearsonr(rob_df["score"], bert_df["score"])
 
-	out_df["BERT distribution"] = ["mean" + bert_mean, "variance" + bert_var, "standard deviation" + bert_std]
-	out_df["RoBERTa distribution"] = ["mean" + rob_mean, "variance" + rob_var, "standard deviation" + rob_std]
-	out_df["correlation"] = ["correlation" + correlation, "p value" + p_val]
+		print(bert_mean.std())
 
-	out_df.to_csv(args.stats_out)
+		out_df["BERT distribution"] = ["mean" + str(bert_mean.mean()), "variance" + str(bert_mean.interval()), "standard deviation" + str(bert_mean.std())]
+		out_df["RoBERTa distribution"] = ["mean" + str(rob_mean.mean()), "variance" + str(rob_mean.interval()), "standard deviation" + str(rob_mean.std())]
+		out_df["correlation"] = ["correlation" + str(correlation), "p value" + str(p_val)]
 
-#	with gzip.open(args.bert_in, "rt") as bert_in, gzip.open(args.roberta_in, "rt") as rob_in, open(args.stats_out, "wt") as s_out:
-#		out_writer = csv.writer(s_out)
-#		out_writer.writerow(["variance", "correlation"])
-#		for b_line, r_line in zip(bert_in, rob_in):
-#			bert_line = json.loads(b_line)
-#			rob_line = json.loads(r_line)
-#			print(bert_line["score"])
-#			bert_mean, bert_var, bert_std = stats.mvsdist(bert_line["score"])
-#			rob_mean, rob_var, rob_std = stats.mvsdist(rob_line["score"])
-#			correlation, p_val = stats.pearsonr(rob_line["score"], bert_line["score"])
-#			out_writer.writerow({"bert mean": bert_mean, "roberta mean": rob_mean, "bert variance": bert_var, "roberta variance": rob_var, "bert standard deviation": bert_std, "roberta standard deviation": rob_std, "correlation": correlation, "p value": p_val} + "\n")
+		out_df.to_csv(args.stats_out)
