e54475a33fdc4d08da963bfc5bd5627a59577ed4
MarleneSchaff <mschaf11@jh.edu>
2025-01-27 16:39:16 +0000

    adding negative scripts

---
+++ scripts/bert_negatives.py
@@ -0,0 +1,19 @@
+import argparse
+import gzip
+import json
+import csv
+import pandas as pd
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("verbs_in", help = "gz.jsonl with verbs")
+	parser.add_argument("neg_out", help = "csv with bert lowest negative score lines")
+
+	args = parser.parse_args()
+
+	df = pd.read_json(args.verbs_in, lines=True, compression="gzip")
+
+	df["score"] = pd.to_numeric(df["score"], errors="coerce")
+	df = df[df["score"] <= -1]
+
+	df.to_csv(args.neg_out)
